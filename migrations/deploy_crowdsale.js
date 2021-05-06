48d3110e08ec0e382b7795edad2d5e3aea144ce0
cpbeamer <77639980+cpbeamer@users.noreply.github.com>
2021-05-06 12:41:45 -0400

    Delete migrations directory

--- migrations/deploy_crowdsale.js
+++
@@ -1,8 +0,0 @@
-const Krytek = artifacts.require("./Kryptek.sol");
-
-module.exports = function(deployer) {
-    const name = "Kryptek";
-    const symbol = "KTEK";
-    deployer.deploy(Krytek, name, symbol);
-}
-
